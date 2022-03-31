1aec57cc6b275f82a032cb78d6fed0644056edfb
Benjamin Idewor <benjaminidewor@gmail.com>
2022-03-31 22:15:46 +0100

    feat: update estate_property views

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -5,21 +5,21 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
 
-    name = fields.Char(required=True, )
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
-    post_code = fields.Char()
-    date_availability = fields.Datetime("Date Availability",
+    post_code = fields.Char(string="Postal Code")
+    date_availability = fields.Datetime("Available From",
                                         default=lambda self: fields.Datetime.now())
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
-        string="Type",
+        string="Garden Orientation",
         selection=[("north", "North"), ("south", "South"),
                    ("east", "East"), ("west", "West")],
         help="This is used to select the garden orientation"
@@ -28,7 +28,7 @@ class EstateProperty(models.Model):
     state = fields.Selection(
         string="State",
         selection=[("New", "New"), ("Offer Received", "Offer Received"),
-                   ("Offer Received", "Offer Accepted"), ("Sold", "Sold"), ("Canceled", "Canceled")],
+                   ("Offer Accepted", "Offer Accepted"), ("Sold", "Sold"), ("Canceled", "Canceled")],
         required=True,
         copy=False,
         default="New"
